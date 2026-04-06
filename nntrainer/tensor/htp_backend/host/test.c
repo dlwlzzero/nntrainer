c82024dedfa0cb9320f543aa387b1c9d3112fe2e
j2z0.lee <j2z0.lee@samsung.com>
2026-04-06 14:01:29 +0900

    remove unused function
    Signed-off-by: j2z0.lee <j2z0.lee@samsung.com>

--- nntrainer/tensor/htp_backend/host/test.c
+++ nntrainer/tensor/htp_backend/host/test.c
@@ -28,37 +28,6 @@ static inline double rand_01() {
   return ((double) rand()) / RAND_MAX;
 }
 
-// assert p_buf, p_fd and size are always valid
-// int alloc_shared_mem_buf(void **p_buf, int *p_fd, size_t size) {
-//   void *buf = rpcmem_alloc(RPCMEM_HEAP_ID_SYSTEM, RPCMEM_FLAG_UNCACHED, size);
-//   if (!buf) {
-//     fprintf(stderr, "alloc_shared_mem_buf: rpcmem_alloc failed\n");
-//     return -1;
-//   }
-
-//   int fd = rpcmem_to_fd(buf);
-//   if (fd < 0) {
-//     fprintf(stderr, "alloc_shared_mem_buf: rpcmem_to_fd failed\n");
-//     return -1;
-//   }
-
-//   // map buffer to the DSP
-//   int err = fastrpc_mmap(CDSP_DOMAIN_ID, fd, buf, 0, size, FASTRPC_MAP_FD);
-//   if (err) {
-//     fprintf(stderr, "alloc_shared_mem_buf: fastrpc_mmap failed, err: %d\n", err);
-//     return -1;
-//   }
-
-//   *p_buf = buf;
-//   *p_fd  = fd;
-//   return 0;
-// }
-
-// void free_shared_mem_buf(void *buf, int fd, size_t size) {
-//   fastrpc_munmap(CDSP_DOMAIN_ID, fd, buf, size);
-//   rpcmem_free(buf);
-// }
-
 static void rms_norm_f32_ref(float *dst, const float *src, int ne0, int ne1) {
   const float eps = 1e-5;
 
@@ -455,9 +424,11 @@ int main(int argc, char **argv) {
 
   // test_mat_mul_rpc(get_global_handle());
 
-  test_mat_mul_qk_0_rpc(get_global_handle());
+  // test_mat_mul_qk_0_rpc(get_global_handle());
 
   // htp_ops_test_ops(get_global_handle());
+
+  test_rms_norm_f32_rpc(get_global_handle(), 60000);
 
   /*
   test_rms_norm_f32_rpc(get_global_handle(), 60000);
